ea1ef68bc070796b1ddcb5646917b560712e1ece
Sivcan Singh <sivcansingh@me.com>
2016-10-06 10:19:42 +0530

    UI and buttons ready

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button: UIButton!
+    var isPlayer1:  Bool  = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("TicTacToe Initialized")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.statusBarStyle = .lightContent
+        
     }
+    
+    var combination = [ [0,3,6] , [1,4,7], [2,5,8] , [0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6] ]
+    @IBAction func playerPressed(_ sender: AnyObject) {
+        
+        print("Button Pressed, success! " + " \(sender.tag!)")
+      
+        if isPlayer1 {
+            if sender.image == #imageLiteral(resourceName: "Circle") {
+                print("It's a circle!")
+            }
+            sender.setImage(#imageLiteral(resourceName: "Circle"), for: [])
+            isPlayer1 = !isPlayer1
+        }
+        else {
+            sender.setImage(#imageLiteral(resourceName: "Cross"), for: [])
+            isPlayer1 = !isPlayer1
+        }
+    }
+    
 
-
+   
 }
 
